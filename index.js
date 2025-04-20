c254e6b5c254a3ae3b1ba081e3728511743257bc
Manuel Maldonado <19945996+Glitchr@users.noreply.github.com>
2025-04-19 20:30:41 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+// app.use(express.static("dist"));
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Phonebook</h1>");
+});
+
+app.get("/info", (req, res) => {
+  const date = new Date();
+  const personCount = persons.length;
+  res.send(`<p>Phonebook has info for ${personCount} people</p><p>${date}</p>`);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).json({ error: "Person not found" });
+  }
+});
+
+app.put("/api/persons/:id", (req, res) => {});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  const newId = Math.floor(Math.random() * 1000000).toString();
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "Name or number is missing",
+    });
+  }
+
+  if (persons.some((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: newId,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3001");
+});
